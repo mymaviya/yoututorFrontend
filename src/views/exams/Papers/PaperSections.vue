0dfc41a995cedf4145202c8066a23dea399f0532
mudassirprnc <mudassirprnc@gmail.com>
2026-05-08 18:19:22 +0530

    updated build

---
+++ src/views/exams/Papers/PaperSections.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="paper-preview">
+    
+        <h2>Paper Section</h2>
+    
+      
+    </div>
+</template>
---
+++ src/views/exams/Papers/QuestionSelector.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="paper-preview">
+    
+        <h2>Question Selector</h2>
+    
+      
+    </div>
+</template>
