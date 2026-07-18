65d0faae87ba4d8b1bc1c8871452b434c14ebd3f
Mudassir Husain <47500865+mymaviya@users.noreply.github.com>
2026-07-18 22:26:01 +0530

    feat(timetable): add summary cards component

---
+++ src/components/timetable/SummaryCards.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  totalPeriods: {
+    type: Number,
+    default: 0,
+  },
+  assignedClasses: {
+    type: Number,
+    default: 0,
+  },
+  subjectsHandled: {
+    type: Number,
+    default: 0,
+  },
+  freePeriods: {
+    type: Number,
+    default: 0,
+  },
+  weeklyCapacity: {
+    type: Number,
+    default: 0,
+  },
+  loading: {
+    type: Boolean,
+    default: false,
+  },
