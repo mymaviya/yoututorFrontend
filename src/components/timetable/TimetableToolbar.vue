4c516fd7552a6c78d35e0396ee0e64ee67705a71
Mudassir Husain <47500865+mymaviya@users.noreply.github.com>
2026-07-18 22:22:42 +0530

    Add timetable toolbar component

---
+++ src/components/timetable/TimetableToolbar.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-card class="timetable-toolbar" rounded="xl" variant="flat">
+    <v-card-text class="pa-4 pa-md-5">
+      <div class="d-flex flex-column ga-4">
+        <div class="d-flex flex-column flex-lg-row align-lg-center justify-space-between ga-3">
+          <div>
+            <div class="text-h6 font-weight-bold">Teacher Timetable</div>
+            <div class="text-body
