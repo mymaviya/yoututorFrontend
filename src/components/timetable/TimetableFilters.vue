619e9d0a733cbab94efa91f0685d3b75352f58fb
Mudassir Husain <47500865+mymaviya@users.noreply.github.com>
2026-07-18 22:24:27 +0530

    feat(timetable): add reusable timetable filters

---
+++ src/components/timetable/TimetableFilters.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  modelValue: {
+    type: Object,
+    default: () => ({})
+  },
+  academicYears: {
+    type: Array,
+    default: () => []
+  },
+  schools: {
+    type: Array,
+    default: () => []
+  },
+  grades: {
+    type: Array,
+    default: () => []
+  },
+  sections: {
+    type: Array,
+    default: () => []
+  },
+  streams: {
+    type: Array,
+    default: () => []
+  },
+  subjects:
