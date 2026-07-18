54b7bb3afbba28fb806476592ff00db603f81a5c
Mudassir Husain <47500865+mymaviya@users.noreply.github.com>
2026-07-18 22:29:20 +0530

    Add reusable timetable legend component

---
+++ src/components/timetable/TimetableLegend.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card
+    class="timetable-legend"
+    rounded="lg"
+    variant="outlined"
+  >
+    <v-card-text class="pa-4">
+      <div class="d-flex flex-wrap align-center ga-3">
+        <div
+          v-if="showTitle"
+          class="text-subtitle-2 font-weight-semibold me-1"
+        >
+          {{ title }}
+        </div>
+
+        <div
+          v-for="item in resolvedItems"
+          :key="item.key"
+          class="legend-item d-inline-flex align-center"
+        >
+          <span
+            class="legend-indicator"
+            :class="[`legend-indicator--${item.shape || 'dot
